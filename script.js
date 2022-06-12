602dbe6604b073cd3092eafb31d94d0c3125ab30
ViniPorto <leinateporto@gmail.com>
2022-06-11 22:13:18 -0300

    adicionado css

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const preencher_formulario = (endereco) => {
 
 const pesquisar_cep = async () => {
     const cep = document.getElementById("cep").value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     const dados = await fetch(url);
     const endereco = await dados.json();
     if(!endereco.hasOwnProperty('erro')){
